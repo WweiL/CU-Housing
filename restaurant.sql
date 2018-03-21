6b52bb832a560db10aa8cad9d6ffb8081db4fa61
BochaoLi <34929442+BochaoLi@users.noreply.github.com>
2018-03-21 10:17:09 +0800

    1

--- restaurant.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists restaurant;
-create table restaurant (
-  building_name varchar(20) not NULL 'home',
-  location varchar(40) not NULL 'home',
-  info char(100) not NULL 'none',
-  PRIMARY KEY (building_name)
-);
